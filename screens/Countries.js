9d61638d052a76f1cfa2fb6686e7cc1d2272aa56
Naomi Todd <naomiunatodd@gmail.com>
2020-04-30 14:36:00 +0100

    updated countries placeholder

--- screens/Countries.js
+++ screens/Countries.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ImageBackground, StyleSheet, View, Text } from 'react-native';
+
+const image = {
+  uri:
+    'https://cutewallpaper.org/21/world-map-iphone-wallpaper/The-World-iPhone-Wallpaper-iDesign-iPhone.jpg'
+};
 
 export default class Countries extends React.Component {
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}
-      >
-        <Text>Countries here!</Text>
+      <View style={styles.container}>
+        <ImageBackground source={image} style={styles.image}>
+          <Text style={styles.text}>Countries here!</Text>
+        </ImageBackground>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
+  },
+  text: {
+    color: 'grey',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+});
