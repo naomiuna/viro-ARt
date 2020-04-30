0b9cb485d4e46e16e2efc023aa26987bfa8e3bd3
Naomi Todd <naomiunatodd@gmail.com>
2020-04-30 13:49:10 +0100

    added AR to basic navigation

--- App.js
+++ App.js
@@ -1,35 +1,13 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
-
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-      </View>
-    );
-  }
-}
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
-}
+import HomeScreen from './screens/Home';
+import AR from './screens/AR';
 
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen
+    AR: AR
   },
   {
     initialRouteName: 'Home'
---
+++ screens/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to AR"
+          onPress={() => this.props.navigation.navigate('AR')}
+        />
+      </View>
+    );
+  }
+}
