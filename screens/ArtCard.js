4359c16749c242398dda9ec7c3ee30af57126bfc
JPReynolds <JordanReynolds@jordans-mbp-3.home>
2020-05-05 11:08:10 +0100

    post selected art to api

--- screens/ArtCard.js
+++ screens/ArtCard.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, Image, View, StyleSheet, Button } from 'react-native';
+import * as api from '../utils/apiAWS.js';
 
-const ArtCard = (props) => {
-  const artObject = props.navigation.getParam('artObject', 'default-value');
-  return (
-    <View style={styles.container}>
-      <Image
-        style={{ width: 130, height: 200 }}
-        source={{ uri: artObject.primaryImage }}
-      />
-      <View>
-        <Text>Title: {artObject.title}</Text>
-        <Text>Artist: {artObject.artistDisplayName}</Text>
-        <Text>Date: {artObject.objectDate}</Text>
-        <Text>
-          Period art was created:
-          {artObject.period.length === 0 ? 'N/A' : artObject.period}
-        </Text>
-        <Text>Art location: {artObject.repository}</Text>
+class ArtCard extends Component {
+  state = { username: 'something97', artObject: {} };
+
+  componentDidMount() {
+    this.setState({
+      artObject: this.props.navigation.getParam('artObject', 'default-value'),
+    });
+  }
+
+  render() {
+    const { artObject } = this.state;
+    return (
+      <View style={styles.container}>
+        <Image
+          style={{ width: 130, height: 200 }}
+          source={{ uri: artObject.primaryImage }}
+        />
+        <View>
+          <Text>Title: {artObject.title}</Text>
+          <Text>Artist: {artObject.artistDisplayName}</Text>
+          <Text>Date: {artObject.objectDate}</Text>
+          <Text>Art location: {artObject.repository}</Text>
+        </View>
+        <Button title="Save Art to Gallery" onPress={this.addArt} />
       </View>
-      <Button title="Save Art to Gallery" />
-    </View>
-  );
-};
+    );
+  }
 
+  addArt = () => {
+    const { username, artObject } = this.state;
+    const body = {
+      username,
+      image_url: artObject.primaryImage,
+      id: artObject.objectID.toString(),
+    };
+    return api
+      .postUserArt(body)
+      .then(() => console.log('artAdded'))
+      .catch((err) => console.log(err));
+  };
+}
 const styles = StyleSheet.create({
   container: { flex: 1 },
 });
