464c37a4ec38d35723c763e9ffc883f25c839467
JPReynolds <JordanReynolds@jordans-mbp-3.home>
2020-05-05 14:27:18 +0100

    added get request for user images from api

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
 import { StyleSheet, Button, View, Text } from 'react-native';
 import ArtList from '../components/ArtList';
+import * as api from '../utils/apiAWS';
 
 export default class ProfileScreen extends Component {
   state = {
-    username: 'naomi',
-    artData: [
-      {
-        primaryImage:
-          'https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg',
-        objectID: '436535'
-      },
-      {
-        primaryImage:
-          'https://images.metmuseum.org/CRDImages/ep/original/DP346474.jpg',
-        objectID: '436528'
-      },
-      {
-        primaryImage:
-          'https://images.metmuseum.org/CRDImages/ep/original/DT1502_cropped2.jpg',
-        objectID: '436532'
-      }
-    ]
+    username: 'something97',
+    artData: [],
   };
+
+  componentDidMount() {
+    api.getUserArt().then((artData) => {
+      const formattedArtData = artData.map((art) => {
+        return { primaryImage: art.image_url, objectID: art.id };
+      });
+      this.setState({ artData: formattedArtData });
+    });
+  }
 
   //componentDidMount  - get username of authenticated user & get  their favourite art
   //artData - need to save all relevant art info for click to work
@@ -35,7 +29,7 @@ export default class ProfileScreen extends Component {
             padding: 16,
             fontSize: 20,
             color: 'white',
-            backgroundColor: '#fb5b5a'
+            backgroundColor: '#fb5b5a',
           }}
         >
           {username}'s Gallery
@@ -53,6 +47,6 @@ export default class ProfileScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 22
-  }
+    paddingTop: 22,
+  },
 });
