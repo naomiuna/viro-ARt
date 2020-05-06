1effa6a001cff1b04389a216fd6e16c51ddcded7
Sam Rowland <samrowland1@Sams-MacBook-Pro.local>
2020-05-06 14:34:12 +0100

    vertical plane selector enabled (buggy)

--- components/ARScene.js
+++ components/ARScene.js
@@ -1,21 +1,23 @@
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 import {
   ViroARScene,
   ViroText,
   ViroConstants,
   ViroARPlaneSelector,
-  ViroImage
-} from 'react-viro';
+  ViroImage,
+  ViroBox,
+  ViroNode,
+} from "react-viro";
 
 export default class ARScene extends Component {
   state = {
-    text: 'Initializing AR...',
-    loading: true
+    text: "Initializing AR...",
+    loading: true,
     // chosenArt: 'https://images.metmuseum.org/CRDImages/ep/original/DP346474.jpg'
   };
 
@@ -23,25 +25,28 @@ export default class ARScene extends Component {
     const { text, loading } = this.state;
     const {
       sceneNavigator: {
-        viroAppProps: { chosenArt }
-      }
+        viroAppProps: { chosenArt },
+      },
     } = this.props;
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized}>
-        {loading ? (
-          <ViroText
-            text={text}
-            scale={[0.5, 0.5, 0.5]}
-            position={[0, 0, -1]}
-            style={styles.helloWorldTextStyle}
-          />
-        ) : (
-          <ViroImage
-            scale={[0.5, 0.5, 0.5]}
-            position={[0, 0, -1]}
-            source={{ uri: chosenArt }}
-          />
-        )}
+      <ViroARScene
+        onTrackingUpdated={this._onInitialized}
+        anchorDetectionTypes="PlanesVertical"
+      >
+        <ViroARPlaneSelector alignment="Vertical">
+          <ViroNode
+            position={[0, 0, 0]}
+            dragType="FixedToWorld"
+            onDrag={() => {}}
+          >
+            <ViroImage
+              scale={[0.5, 0.5, 0.5]}
+              position={[1, 0, -1]}
+              rotation={[-90, -90, 0]}
+              source={{ uri: chosenArt }}
+            />
+          </ViroNode>
+        </ViroARPlaneSelector>
       </ViroARScene>
     );
   }
@@ -49,22 +54,47 @@ export default class ARScene extends Component {
   _onInitialized = (state, reason) => {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        loading: false
+        loading: false,
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
   };
+  onSelect = () => {
+    return (
+      <ViroImage
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        source={{ uri: chosenArt }}
+      />
+    );
+  };
 }
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
-    fontFamily: 'Arial',
+    fontFamily: "Arial",
     fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center'
-  }
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center",
+  },
 });
 
 module.exports = ARScene;
+
+// {loading ? (
+//   <ViroText
+//     text={text}
+//     scale={[0.5, 0.5, 0.5]}
+//     position={[0, 0, -1]}
+//     style={styles.helloWorldTextStyle}
+//   />
+// ) : (
+//   <ViroARPlaneSelector />
+//   <ViroImage
+//     scale={[0.5, 0.5, 0.5]}
+//     position={[0, 0, -1]}
+//     source={{ uri: chosenArt }}
+//   />
+// )}
