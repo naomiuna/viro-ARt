e9736797450c92d434d0691c9029ba72f66b6563
Naomi Todd <naomiunatodd@gmail.com>
2020-05-07 18:42:48 +0100

    adds fullscreen view of art from profile

--- components/ArtList.js
+++ components/ArtList.js
@@ -4,30 +4,72 @@ import {
   FlatList,
   TouchableOpacity,
   Image,
-  StyleSheet
+  StyleSheet,
+  Modal
 } from 'react-native';
 
 class ArtList extends Component {
   state = {
-    refreshing: false
+    refreshing: false,
+    ModalVisibleStatus: false,
+    modalImgURI: ''
   };
   render() {
-    return (
-      <FlatList
-        data={this.props.art}
-        numColumns={3}
-        keyExtractor={this.keyExtractor}
-        renderItem={this.renderItem}
-        refreshing={this.state.refreshing}
-        onRefresh={() => {
-          if (this.props.type === 'profile') {
-            this.props.fetchArt();
-          }
-        }}
-      />
-    );
+    if (this.state.ModalVisibleStatus) {
+      return (
+        <Modal
+          transparent={false}
+          animationType={'fade'}
+          visible={this.state.ModalVisibleStatus}
+          onRequestClose={() => {
+            this.showModalFunction(false, '');
+          }}
+        >
+          <View style={styles.modelStyle}>
+            <Image
+              style={styles.fullImageStyle}
+              source={{ uri: this.state.modalImgURI }}
+            />
+            <TouchableOpacity
+              activeOpacity={0.5}
+              style={styles.closeButtonStyle}
+              onPress={() => {
+                this.showModalFunction(false, '');
+              }}
+            >
+              <Image
+                source={{
+                  uri:
+                    'https://raw.githubusercontent.com/AboutReact/sampleresource/master/close.png'
+                }}
+                style={{ width: 35, height: 35, marginTop: 16 }}
+              />
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      );
+    } else {
+      return (
+        <FlatList
+          data={this.props.art}
+          numColumns={3}
+          keyExtractor={this.keyExtractor}
+          renderItem={this.renderItem}
+          refreshing={this.state.refreshing}
+          onRefresh={() => {
+            if (this.props.type === 'profile') {
+              this.props.fetchArt();
+            }
+          }}
+        />
+      );
+    }
   }
   keyExtractor = (item) => item.objectID.toString();
+
+  showModalFunction = (ModalVisibleStatus, modalImgURI) => {
+    this.setState({ ModalVisibleStatus, modalImgURI });
+  };
 
   renderItem = ({ item }) => (
     <View style={{ flex: 1, flexDirection: 'column', margin: 2 }}>
@@ -39,6 +81,8 @@ class ArtList extends Component {
             this.props.navigation.navigate('ArtCard', {
               artObject: item
             });
+          } else {
+            this.showModalFunction(true, item.primaryImage);
           }
         }}
       >
@@ -56,6 +100,26 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: 120
+  },
+  fullImageStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '98%',
+    resizeMode: 'contain'
+  },
+  modelStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.4)'
+  },
+  closeButtonStyle: {
+    width: 25,
+    height: 25,
+    top: 9,
+    right: 9,
+    position: 'absolute'
   }
 });
 
